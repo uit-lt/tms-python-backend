8255d31a9311859abb65ac5e08ee1350c3ebe21a
Tan Nguyen <tannp27@gmail.com>
2025-03-26 20:08:35 +0700

    #3 feat: update default values for Flask and MySQL environment variables in config.py

--- config.py
+++ config.py
@@ -4,13 +4,14 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv(override=True)
 
-FLASK_HOST = os.getenv("FLASK_HOST", "flask_app")
-FLASK_HOST_PORT = os.getenv("FLASK_PORT", "5000")
+# Set default values for environment variables (in docker container)
+FLASK_HOST = os.getenv("FLASK_HOST", "0.0.0.0")
+FLASK_PORT = os.getenv("FLASK_PORT", "5000")
 
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("MYSQL_ROOT_PASSWORD")
-DB_HOST = os.getenv("DB_HOST", "db")  # Default to localhost if not set
-DB_PORT = os.getenv("DB_PORT", "3306")  # Default to 3306 if not set
+DB_HOST = os.getenv("DB_HOST", "db")  # Default to db if not set in the docker-compose file
+DB_PORT = os.getenv("DB_PORT", "3306")  # Default to 3306 if not set in the docker-compose file
 DB_NAME = os.getenv("DB_NAME")
 
 SQLALCHEMY_DATABASE_URI = os.getenv(
