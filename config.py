db74093e27498deffa1ea63c5ea20f61c23977c8
Tan Nguyen <tannp27@gmail.com>
2025-04-02 21:27:02 +0700

    feat: update default database credentials in config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 # Load environment variables from .env file
@@ -9,15 +8,10 @@ load_dotenv(override=True)
 # Set default values for environment variables (in docker container)
 FLASK_HOST = os.getenv("FLASK_HOST", "0.0.0.0")
 FLASK_PORT = os.getenv("FLASK_PORT", "5000")
-
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("MYSQL_ROOT_PASSWORD")
-DB_HOST = os.getenv(
-    "DB_HOST", "db"
-)  # Default to db if not set in the docker-compose file
-DB_PORT = os.getenv(
-    "DB_PORT", "3306"
-)  # Default to 3306 if not set in the docker-compose file
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("MYSQL_ROOT_PASSWORD", "root")
+DB_HOST = os.getenv("DB_HOST", "db")
+DB_PORT = os.getenv("DB_PORT", "3306")
 DB_NAME = os.getenv("DB_NAME")
 
 SQLALCHEMY_DATABASE_URI = os.getenv(
@@ -27,8 +21,5 @@ SQLALCHEMY_DATABASE_URI = os.getenv(
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = SQLALCHEMY_TRACK_MODIFICATIONS
+# Initialize SQLAlchemy without app
 db = SQLAlchemy()
-db.init_app(app)
