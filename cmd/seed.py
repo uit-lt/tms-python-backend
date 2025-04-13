11fd18d39814569418cac2213e584b1e512f772f
thuankg1752 <thuankg1752@gmail.com>
2025-04-14 01:48:32 +0700

    #5 feat: implement user seeding with Faker for generating unique users

--- cmd/seed.py
+++ cmd/seed.py
@@ -1,27 +1,36 @@
+from faker import Faker
 from app import create_app
 from app.helpers.extensions import db
+from app.models import User
 from werkzeug.security import generate_password_hash
 
-from app.models import User
-
+fake = Faker()
 app = create_app()
 
+PASSWORD = "123456Aa@"
+
+def user_exists(email, username):
+    return User.query.filter((User.email == email) | (User.username == username)).first()
+
 with app.app_context():
+    users = []
 
-    # Seed user
-    user1 = User(
-        username="admin",
-        email="admin@example.com",
-        password_hash=generate_password_hash("123456Aa@")
-    )
-    user2 = User(
-        username="user",
-        email="user@example.com",
-        password_hash=generate_password_hash("123456Aa@")
-    )
-    db.session.add_all([user1, user2])
+    for _ in range(10):
+        while True:
+            username = fake.user_name()
+            email = fake.unique.email()
+
+            if not user_exists(email, username):
+                break
+
+        user = User(
+            username=username,
+            email=email,
+            password_hash=generate_password_hash(PASSWORD)
+        )
+        users.append(user)
+
+    db.session.add_all(users)
     db.session.commit()
 
-    db.session.commit()
-
-    print("✅ Seed data inserted!")
+    print("✅ Seeded 10 fake users successfully.")
